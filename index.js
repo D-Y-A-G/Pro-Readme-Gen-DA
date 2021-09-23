004bec01831dba3bf353d894ccf19cb5dbf4efc0
Denis Arce <dnsarce21@gmail.com>
2021-09-23 11:41:51 -0500

    added questions for prompt using inquirer

--- index.js
+++ index.js
@@ -1,9 +1,56 @@
 /////////setup required variables/////////////
 
 const inquirer = require("inquirer");
-const fs = require('fs');
+const fs = require("fs");
 
+///////////Set questions for README with inquirer and .then to get answers///////////////
 
+inquirer;
+prompt([
+  {
+    type: "input",
+    message: "What was your motivation?",
+    name: "motivation",
+  },
 
+  {
+    type: "input",
+    message: "Why did you build this Project?",
+    name: "reason",
+  },
 
+  {
+    type: "input",
+    message: "What problem does it solve?",
+    name: "problem",
+  },
 
+  {
+    type: "input",
+    message: "What did you learn?",
+    name: "learned",
+  },
+
+  {
+    type: "input",
+    message: "What makes your Project stand out?",
+    name: "unique",
+  },
+
+  {
+    type: "input",
+    message: "What were some challenges you experienced?",
+    name: "challenges",
+  },
+
+  {
+    type: "input",
+    message: "What features will you implement in the future?",
+    name: "future",
+  },
+])
+  .then(
+    async (answers) =>
+      await fs.writeFileSync("README.md", generateReadme(userInput))
+  )
+  .then(() => console.log("README Successfully Created!"));
