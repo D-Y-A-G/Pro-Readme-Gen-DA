a768daaec7bc221bd92c4a7b87799b579aee38b9
Denis Arce <dnsarce21@gmail.com>
2021-09-24 20:57:37 -0500

    working on license badges and format

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
-# **🏆BLah🏆** </br>
+# **🏆qojwhg'wrqgwqgw🏆** </br>
 
 
   ## Project Description
   
-  BLah blah
+  ```md
+  gqwegwqgqweg
+  ```
 
   ---  
  
   ## Table of Contents
+
   
-  -[Installation](#installation) 
+  -[Installation](#installation)
 
   -[Usage](#usage) 
 
@@ -18,69 +21,89 @@
   -[License](#license) 
 
   -[Features](#features) 
-  
+
   -[Questions](#questions)
 
   ---
   
   ## Installation
   
-  DOwnlad repo
+  ```md
+  gqgwrgrgq
+  ```
   
  ---
   
   ## Usage
   
-  terminal
+  ```md
+  grrgqr
+  ```
   
   ---
 
   To add a screenshot, create an 'assets/images' folder in your repository and upload your screenshot to it. Then, using the relative filepath.
   
   ## Credits
-
-  ![collaborators](https://img.shields.io/npm/collaborators/inquirer)]
- 
+  ```md
+  ![collaborators](https://img.shields.io/npm/collaborators/inquirer)
+  ```
   ---
   
-  Me myself
+  ```md
+  grqegrwwg
+  ```
   
   
   ## License
-  
-  APACHE, BSD , MIT , GNU 
+
+  ```md  
+  Apache, BSD , MIT
+  ```
   
   ---
   
   ## 🌟Badges🌟
   
+ 
   ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
   
   ![GitHub Stars](https://img.shields.io/github/stars/dnsarce21/Pro-Readme-Gen-DA?style=social)
   
   ![followers](https://img.shields.io/github/followers/dnsarce21?style=social)
-
+ 
   ---
 
   ## Features
   
-  None
+  ```md
+  gwrgrw
+  ```
 
   ---
 
   ## How to contribute
   
+ 
   [![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](code_of_conduct.md)
   ![Contributors](https://img.shields.io/github/contributors/dnsarce21/Pro-Readme-Gen-DA)
-
+  
   ---
   
   ## Tests
   
+  ```md
   Write tests for your application and provide examples on how to run them here.
-
+  ```
   ---
   
   ## Questions
+
   
-  🥇For any questions please contact me on GitHub: https://github.com/dnsarce21 or email me @: denisarce@hotmail.com
+  🥇For any questions please contact me on:
+  
+  GitHub: [dnsarce21](https://github.com/dnsarce21)
+  
+  Email me: dnsarce21@me.com
+ 
+  
--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 
 const inquirer = require("inquirer");
 const fs = require("fs");
+const apache =
+  "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+const mit =
+  "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+const gnu =
+  "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"; 
 
 ///////////Set questions for README with inquirer and .then to get answers///////////////
 
@@ -42,7 +48,7 @@ inquirer
       type: "checkbox",
       message: "Please choose a license for your project!",
       name: "license",
-      choices: ["APACHE", " BSD ", " MIT ", " GNU "],
+      choices: ["Apache", " BSD ", " MIT", " GNU "],
     },
 
     {
@@ -72,16 +78,24 @@ inquirer
 ////////////////function to write README.md file/////////////////////////////
 
 const generateReadme = function (writeFile) {
-  const readMe = `# **🏆${writeFile.title}🏆** </br>
+  const readMe =
+    `# **🏆${writeFile.title}🏆** </br>
 
 
   ## Project Description
   
+  ` +
+    "```md" +
+    `
   ${writeFile.description}
+  ` +
+    "```" +
+    `
 
   ---  
  
   ## Table of Contents
+
   
   -[Installation](#installation)
 
@@ -99,69 +113,117 @@ const generateReadme = function (writeFile) {
   
   ## Installation
   
+  ` +
+    "```md" +
+    `
   ${writeFile.installation}
+  ` +
+    "```" +
+    `
   
  ---
   
   ## Usage
   
+  ` +
+    "```md" +
+    `
   ${writeFile.usage}
+  ` +
+    "```" +
+    `
   
   ---
 
   To add a screenshot, create an 'assets/images' folder in your repository and upload your screenshot to it. Then, using the relative filepath.
   
   ## Credits
-
-  ![collaborators](https://img.shields.io/npm/collaborators/inquirer)]
- 
+  ` +
+    "```md" +
+    `
+  ![collaborators](https://img.shields.io/npm/collaborators/inquirer)
+  ` +
+    "```" +
+    `
   ---
   
+  ` +
+    "```md" +
+    `
   ${writeFile.collaborators}
+  ` +
+    "```" +
+    `
   
   
   ## License
-  
+
+  ` +
+    "```md" +
+    `  
   ${writeFile.license}
+  ` +
+    "```" +
+    `
   
   ---
   
   ## 🌟Badges🌟
   
+ 
   ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
   
   ![GitHub Stars](https://img.shields.io/github/stars/dnsarce21/Pro-Readme-Gen-DA?style=social)
   
   ![followers](https://img.shields.io/github/followers/dnsarce21?style=social)
-
+ 
   ---
 
   ## Features
   
+  ` +
+    "```md" +
+    `
   ${writeFile.features}
+  ` +
+    "```" +
+    `
 
   ---
 
   ## How to contribute
   
+ 
   [![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](code_of_conduct.md)
   ![Contributors](https://img.shields.io/github/contributors/dnsarce21/Pro-Readme-Gen-DA)
-
+  
   ---
   
   ## Tests
   
+  ` +
+    "```md" +
+    `
   Write tests for your application and provide examples on how to run them here.
-
+  ` +
+    "```" +
+    `
   ---
   
   ## Questions
+
   
-  🥇For any questions please contact me on
+  🥇For any questions please contact me on:
   
-  GitHub: https://github.com/${writeFile.github} 
+  GitHub: [${writeFile.github}]` +
+    "(https://github.com/" +
+    writeFile.github +
+    ")" +
+    `
   
-  email me @: ${writeFile.email}`;
+  Email me: ${writeFile.email}
+ 
+  `;
 
   return readMe;
 };
